b8e74ca216df6ba4cee4578c5b7b958ff9d1342d
unknown <nooruddinansary@gmail.com>
2025-01-27 00:35:15 +0530

    url change

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:4001",
+    origin: "https://chatwebapplication-7uya.onrender.com",
     methods: ["GET", "POST"],
   },
 });
