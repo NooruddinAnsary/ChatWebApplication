33aa07bab7ae8acddd01811e52116c8a303beea5
unknown <nooruddinansary@gmail.com>
2025-01-27 00:26:31 +0530

    3nd commit

--- Backend/Frontend/tailwind.config.js
+++ Backend/Frontend/tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//     theme: {
+//         extend: {},
+//     },
+//     plugins: [require("daisyui")],
+// };
+import daisyui from 'daisyui';
+
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
 };
